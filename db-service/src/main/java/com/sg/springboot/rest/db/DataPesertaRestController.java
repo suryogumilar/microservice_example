f87d44ea6f0ce0d60f0b915419b08ffa929c462f
Suryo Gumilar <suryo.gumilar@gmail.com>
2020-01-20 19:11:18 +0700

    add delete

--- db-service/src/main/java/com/sg/springboot/rest/db/DataPesertaRestController.java
+++ db-service/src/main/java/com/sg/springboot/rest/db/DataPesertaRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sg.mybatis.mapper.DataPesertaMapper;
 import com.sg.mybatis.model.DataPeserta;
 import com.sg.springboot.rest.model.DataPesertaResponse;
-
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/data-peserta")
@@ -25,7 +25,7 @@ public class DataPesertaRestController {
 	private static Logger logger = LogManager.getLogger(DataPesertaRestController.class);
 	@Autowired
 	private DataPesertaMapper dataPesertaMapper;
-	@RequestMapping(name="getDataPeserta",path="/get",method=RequestMethod.GET)
+	@RequestMapping(name="getDataPeserta",path="/get",method=RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public DataPeserta getDataPeserta(@RequestParam("id") Integer id){
 		logger.debug("get Data Peserta with id ="+id);
 		Optional<DataPeserta> dp = dataPesertaMapper.selectByPrimaryKey(id);
@@ -33,14 +33,16 @@ public class DataPesertaRestController {
 		return dataPeserta;
 	}
 	
-	@RequestMapping(name="listDataPeserta",path="/list",method=RequestMethod.GET)
+	@RequestMapping(name="listDataPeserta",path="/list",method=RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<DataPeserta> getDataPeserta(){
 		logger.debug("list data peserta");
 		SelectDSLCompleter allRows = SelectDSLCompleter.allRows();
 		List<DataPeserta> dataPesertaList = dataPesertaMapper.select(allRows);
 		return dataPesertaList;
 	}
-	@RequestMapping(name="addDataPeserta",path="/add",method=RequestMethod.POST)
+	@RequestMapping(name="addDataPeserta",path="/add",method=RequestMethod.POST, consumes = {
+			MediaType.TEXT_PLAIN_VALUE, MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public DataPesertaResponse insertDataPeserta(@RequestBody DataPeserta dataPeserta) {
 		DataPesertaResponse result = new DataPesertaResponse();
 		
@@ -50,6 +52,29 @@ public class DataPesertaRestController {
 		
 		
 		result.setStatus("OK");
+		return result;
+	}
+	
+	@RequestMapping(name="deleteDataPeserta",path="/delete",method=RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public DataPesertaResponse deleteDataPeserta(@RequestParam("id") Integer id){
+		DataPesertaResponse result = new DataPesertaResponse();
+		
+		logger.debug("get Data Peserta with id ="+id);
+		Optional<DataPeserta> dp = dataPesertaMapper.selectByPrimaryKey(id);
+		DataPeserta dataPeserta = dp.get();
+		
+		int deleteByPrimaryKey = dataPesertaMapper.deleteByPrimaryKey(id);
+		if(dataPeserta==null) {
+			result.setStatus("NOT-OK");
+			result.setMessage("Data not found");
+			
+		}else {
+			result.setStatus("OK");
+			result.setData(dataPeserta);
+			result.setMessage("delete row = "+deleteByPrimaryKey);
+		}
+		
+		
 		return result;
 	}
 	
